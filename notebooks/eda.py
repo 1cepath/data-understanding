f1793993779e0cc45162bb45f904387cbfc96f64
emaasit <daniel.emaasit@gmail.com>
2018-04-08 17:32:34 -0400

    prepared template for import & tidy notebook

--- notebooks/eda.py
+++ notebooks/eda.py
@@ -1,14 +1,16 @@
 %matplotlib inline
+%config InlineBackend.figure_format = 'retina'
 import os
 import numpy as np
 import pandas as pd
 import dask.dataframe as dd
 import pickle
-import seaborn as sns
+import seaborn as sns             # for static plots
 import matplotlib.pyplot as plt
-from bqplot import *
+from bqplot import *              # for interactive plots
 from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
+fig_layout = widgets.Layout(width = "100%")
 sns.set_context('notebook', font_scale = 1.1)
 np.random.seed(12345)
 rc = {'xtick.labelsize': 40, 'ytick.labelsize': 40, 'axes.labelsize': 40, 'font.size': 40, 'lines.linewidth': 4.0, 
